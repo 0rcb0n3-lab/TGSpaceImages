0d290b1021b035287033344f54ff3d052d779636
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-04-28 22:40:01 +0300

    Create fetch_apod_images.py

---
+++ fetch_apod_images.py
@@ -0,0 +1,55 @@
+import os
+import argparse
+import requests
+from dotenv import load_dotenv
+
+from download_images import download_image, get_file_format
+
+
+def fetch_apod_images(nasa_token, count):
+    nasa_apod_api = 'https://api.nasa.gov/planetary/apod'
+    params = {'count': count, 'api_key': nasa_token}
+    response = requests.get(nasa_apod_api, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def download_apod_images(images):
+
+    directory = "Space_photos"
+    os.makedirs(directory, exist_ok=True)
+
+    for img_number, image in enumerate(images, start=1):
+        if image.get('media_type') != 'image':
+            continue
+
+        link = image.get('url')
+        extension = get_file_format(link)
+        filename = f'nasa_{img_number}{extension}'
+        filepath = os.path.join(directory, filename)
+
+        download_image(link, filepath)
+
+
+def main():
+    load_dotenv()
+
+    nasa_token = os.getenv('NASA_API_KEY')
+
+    parser = argparse.ArgumentParser(
+        description='Скачивает снимки APOD с NASA'
+    )
+    parser.add_argument('--nasa-token', default=nasa_token, help='API-ключ NASA')
+    parser.add_argument('--count', type=int, default=6, help='Кол-во снимков (по умолчанию 6)')
+    args = parser.parse_args()
+
+    if not args.nasa_token:
+        raise RuntimeError('NASA_API_KEY не найден в .env / не передан через --nasa-token')
+
+    images = fetch_apod_images(args.nasa_token, args.count)
+
+    download_apod_images(images)
+
+
+if __name__ == '__main__':
+    main()
