1f4090a02f9a86a12247d860ed1901c136633cab
0rcb0n3-lab <0rcb0n3@gmail.com>
2026-04-28 22:40:14 +0300

    Create tgbot_post_image.py

---
+++ tgbot_post_image.py
@@ -0,0 +1,26 @@
+import os
+import random
+import telegram
+from dotenv import load_dotenv
+
+
+def main():
+    load_dotenv()
+
+    tgbot_token = os.getenv('TGBOT_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+
+    bot = telegram.Bot(token=tgbot_token)
+
+    directory = 'Space_photos'
+    images = [file for file in os.listdir(directory) if file.lower().endswith(('.jpg', '.png'))]
+
+    random_image = random.choice(images)
+    image_path = os.path.join(directory, random_image)
+
+    with open(image_path, 'rb') as image:
+        bot.send_photo(chat_id=tg_chat_id, photo=image)
+
+
+if __name__ == '__main__':
+    main()
